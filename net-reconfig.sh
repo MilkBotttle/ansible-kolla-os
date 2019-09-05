43c61af122356c803d54e8056814f8cbdb9e4506
cameron.c <cameron.c@inwinstack.com>
2019-09-05 16:44:39 +0800

    Add config deployed node, update create port strategy
    - Add var 'deployed' to create network config for deployed node.
    - Add more condition to create port.
    - Update generate inventory jinja2 template in simple formta.
    - Untracked some files.

--- net-reconfig.sh
+++ net-reconfig.sh
@@ -1,5 +1,11 @@
+if [ "x$@" == "x" ]; then
+    echo "Need apply host. Use bash net-reconfig.sh -e host=hostname_or_group !"
+    echo
+    exit 1
+fi
+
 echo "Create deployed server network info"
-ansible-playbook main.yaml -t create-network -e deployed_server=true -v
+ansible-playbook main.yaml -t create-network -e var_file=deployed.yaml -e deployed_server=true -v
 echo "Reconfig network "
 ansible-playbook -b -i kolla-os-inventory.yaml os-net-reconfig.yaml "$@"
 
